22a79189e19c018b76457af2128464477751dfe1
wi23b145 <wi23b145@technikum-wien.at>
2024-05-29 16:55:06 +0200

    Coding

---
+++ project_tictactoe/src/main/java/at/technikum/com/example/Board.java
@@ -0,0 +1,59 @@
+package at.technikum.com.example;
+
+public class Board {
+
+        private char[][] cells;
+
+        public Board() {
+            cells = new char[3][3];
+            clear();
+        }
+
+        public boolean isCellEmpty(int x, int y) {
+            return cells[x][y] == ' ';
+        }
+
+        public void place(int x, int y, char marker) {
+            if (isCellEmpty(x, y)) {
+                cells[x][y] = marker;
+            }
+        }
+
+        public boolean isFull() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (cells[i][j] == ' ') {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void clear() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    cells[i][j] = ' ';
+                }
+            }
+        }
+
+        public void print() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(cells[i][j]);
+                    if (j < 2) System.out.print(" | ");
+                }
+                System.out.println();
+                if (i < 2) System.out.println("---------");
+            }
+        }
+
+        // Getter-Methode für cells
+        public char[][] getCells() {
+            return cells;
+        }
+    }
+
+
+
---
+++ project_tictactoe/src/main/java/at/technikum/com/example/Player.java
@@ -0,0 +1,15 @@
+package at.technikum.com.example;
+
+public class Player {
+        private char marker;
+
+        public Player(char marker) {
+            this.marker = marker;
+        }
+
+        public char getMarker() {
+            return marker;
+        }
+    }
+
+
